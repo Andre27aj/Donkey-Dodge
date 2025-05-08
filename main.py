8ddd89100e9558b8f6a9ade366dcfa894ade905e
Clément Hebraud <clement.hebraud@efrei.net>
2025-05-08 16:15:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,8 @@
 from menu import *
 
-def run_game():
-    running = True
-    current_screen = None
+def main():
+    
+    menu_principal()
 
-    while running:
-        # Afficher le menu principal
-        from menu import menu_principal
-        menu_screen = menu_principal()
-        current_screen = menu_screen
-
-        # Lancer le jeu
-        from jeu import main_game
-        result = main_game(current_screen)
-
-        # Traiter le résultat
-        if result == "menu":
-            # On continue la boucle pour revenir au menu
-            continue
-        elif result == "quit":
-            running = False
-
-
-# Appel de la fonction principale
 if __name__ == "__main__":
-    run_game()
+    main()
